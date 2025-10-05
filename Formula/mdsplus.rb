4142d57f600963362333e3b5b69be66d3d07c06b
Darren Garnier <garnier@mit.edu>
2025-10-05 13:31:30 -0400

    added testing and fixed permissions by adding missing shebangs.

--- Formula/mdsplus.rb
+++ Formula/mdsplus.rb
@@ -9,6 +9,8 @@ class Mdsplus < Formula
   #url "https://github.com/MDSplus/mdsplus/archive/1d03558.zip"
   #sha256 "244139c73373adb076aa50239282e32c46ef4e04af3bdfd44943546c907f370e"
   head "https://github.com/MDSplus/mdsplus.git", branch: "alpha"
+
+  option "without-tests", "Build without tests"
   
   depends_on "pkg-config" => :build
   depends_on "cmake" => [:build, :test]
@@ -41,14 +43,6 @@ class Mdsplus < Formula
 
   def install
 
-    # Ensure mitdevices uses gtar (from gnu-tar) instead of "cmake -E tar"
-    inreplace "mitdevices/CMakeLists.txt",
-              '${CMAKE_COMMAND} -E tar -czf',
-              '/opt/homebrew/bin/gtar -czf'
-
-    inreplace "setup.sh", '/usr/local/mdsplus', opt_prefix
-    inreplace "setup.csh", '/usr/local/mdsplus', opt_prefix
-
     # Prefer Homebrew-provided X11/Motif headers/libs so the build does not pick
     # up macOS SDK framework headers (e.g. Tk.framework) which can provide
     # incompatible X11/Xlib.h. d
@@ -57,26 +51,69 @@ class Mdsplus < Formula
       -B workspace/build
       -G Ninja
       -DMotif_X11_INCLUDE_DIR=#{Formula["libx11"].opt_include}
-      -DENABLE_HDF5=ON
-      -DHDF5_INCLUDE_DIR=#{Formula["hdf5"].opt_include}
-      -DHDF5_LIBRARY_DIR=#{Formula["hdf5"].opt_lib}
       -DPLATFORM=macosx
       -DCMAKE_INSTALL_PREFIX=#{prefix}
     ]
+
+    #       -DENABLE_HDF5=ON
+    #  -DHDF5_INCLUDE_DIR=#{Formula["hdf5"].opt_include}
+    #  -DHDF5_LIBRARY_DIR=#{Formula["hdf5"].opt_lib}
+
+    ENV["HDF5_DIR"] = Formula["hdf5"].opt_prefix
+
+
+    # Ensure mitdevices uses gtar (from gnu-tar) instead of "cmake -E tar"
+    inreplace "mitdevices/CMakeLists.txt",
+              '${CMAKE_COMMAND} -E tar -czf',
+              '/opt/homebrew/bin/gtar -czf'
+
+    if build.with? "tests"
+      inreplace "python/MDSplus/CMakeLists.txt" do |s|
+        s.gsub!(/PATTERN "tests" EXCLUDE/, '# PATTERN "tests" EXCLUDE')
+      end
+      args = args + %W[
+        -DBUILD_TESTING=ON
+      ] 
+    end
+
+    inreplace [
+        "setup.sh", "setup.csh", 
+        "python/MDSplus/__init__.py",
+        "python/MDSplus/version.py",
+        "python/MDSplus/tree.py",
+        "python/MDSplus/wsgi/__init__.py",
+        "python/MDSplus/wsgi/doMdsip.py",
+        "python/MDSplus/wsgi/conf/mdsplus.wsgi",
+        "epics/archiver/Sdd2Mds",
+        "cmake/FindMDSplus.cmake",
+        "macosx/mdsip.plist",
+        "tdi/treeshr/TreeShrHook.py.example",
+      ], '/usr/local/mdsplus', opt_prefix
+
+
 
     system "cmake", *args
     system "cmake", "--build", "workspace/build", "--", "-j#{ENV.make_jobs}"
     system "cmake", "--install", "workspace/build", "--prefix", "#{prefix}"
 
+    # prepend #!/bin/sh to these files so the get the right permissions
+    # after homebrew "cleans up"
+    inreplace [ bin/"job_finish", bin/"job_functions", 
+                bin/"job_output", bin/"job_start",
+                bin/"mdstcl", bin/"synchronize_unix"
+              ] do |s|
+                s.sub!(/\A(?!#!)/, "#!/bin/sh\n")
+              end
 
-    # install didn't install python tests.. so comment them out 
-    inreplace prefix/"python/MDSplus/pyproject.toml", "'MDSplus.tests'", "#'MDSplus.tests'"
+    # install didn't install python tests.. so comment them out
+    if build.without? "tests" 
+      inreplace prefix/"python/MDSplus/pyproject.toml", "'MDSplus.tests'", "# 'MDSplus.tests'"
+    end
     build_venv = virtualenv_create(buildpath/"venv", "python3.13")
     build_venv.pip_install "wheel"
     ENV.prepend_path "PATH", buildpath/"venv/bin"
-    system "cd", prefix/"python", "&&", "python", "-m", "pip", "wheel", "--no-deps", "./MDSplus"
-    system "rm", "-r", buildpath/"venv"
-
+    system "python3", "-m", "pip", "wheel", #"--no-deps", 
+      "-w", prefix/"python", prefix/"python/MDSplus"
   end
 
   def caveats
@@ -86,22 +123,21 @@ class Mdsplus < Formula
 
       It is recommended to add the following to your .zshrc:
 
-      if [ -f #{opt_prefix}/setup.sh ]; then
-        source #{opt_prefix}/setup.sh
-      fi
+        if [ -f #{opt_prefix}/setup.sh ]; then
+          source #{opt_prefix}/setup.sh
+        fi
     EOS
   end
 
   test do
     # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test MDSplus`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    ENV['MDSPLUS_DIR']=opt_prefix
+    test_venv = virtualenv_create(testpath/"venv", "python3.13")
+    ENV.prepend_path "PATH", testpath/"venv/bin"
+    wheel = Dir[opt_prefix/"python/*.whl"].first
+    system "python3", "-m", "pip", "install", wheel
+    system "python3", "-c", "from unittest import TextTestRunner;" +
+          "from MDSplus.tests import data_case as test_case;" +
+          "TextTestRunner(verbosity=2).run(test_case.Tests.getTestSuite())"
   end
 end
