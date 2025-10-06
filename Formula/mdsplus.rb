c0a9aa3e8055f564229b9cd62282f89a480959d5
Darren Garnier <garnier@mit.edu>
2025-10-06 11:57:53 -0400

    re-arrange some testing stuff and passes more tests.. not passing image and dispatcher testing

--- Formula/mdsplus.rb
+++ Formula/mdsplus.rb
@@ -3,11 +3,11 @@ class Mdsplus < Formula
   desc "The MDSplus data management system"
   homepage "https://mdsplus.org/"
   license "MIT"
-  #url "https://github.com/MDSplus/mdsplus"
-  #url "https://github.com/MDSplus/mdsplus/tree/1d03558.git", :using 
+  #license_files ["MDSplus-license.txt", "MDSplus-license.rtf"]
 
-  #url "https://github.com/MDSplus/mdsplus/archive/1d03558.zip"
-  #sha256 "244139c73373adb076aa50239282e32c46ef4e04af3bdfd44943546c907f370e"
+  url "https://github.com/MDSplus/mdsplus.git", revision: "f11259dec7874a88b2ab0af4456b393923fdf87e"
+  version 'alpha_release-7-153-7'
+
   head "https://github.com/MDSplus/mdsplus.git", branch: "alpha"
 
   option "without-tests", "Build without tests"
@@ -17,17 +17,23 @@ class Mdsplus < Formula
   depends_on "ninja" => [:build, :test]
   depends_on "bison" => [:build]
   depends_on "gnu-tar" => :build
+  depends_on "doxygen" => :build
 
   #uses_from_macos "python" => :build
   uses_from_macos "libffi"
   uses_from_macos "libxml2"
   uses_from_macos "zlib"
   uses_from_macos "libiconv"
-  uses_from_macos "liblzma"
-
+  uses_from_macos "blas"  # use macos Accelerate framework
+  
+  # these dont seem to work, so replace with homebrew-provided ones
+  #uses_from_macos "readline"
+  #uses_from_macos "xz"  # liblzma somehow this isn't getting used
+  
   depends_on "python@3.13" => :build
   depends_on "readline"
-  depends_on "openblas"
+  depends_on "xz"
+  # depends_on "openblas"
   depends_on "freetds"
   depends_on "libx11"
   depends_on "openmotif"
@@ -67,13 +73,15 @@ class Mdsplus < Formula
               '${CMAKE_COMMAND} -E tar -czf',
               '/opt/homebrew/bin/gtar -czf'
 
+    # fix python testing.. either with or without tests
     if build.with? "tests"
-      inreplace "python/MDSplus/CMakeLists.txt" do |s|
-        s.gsub!(/PATTERN "tests" EXCLUDE/, '# PATTERN "tests" EXCLUDE')
-      end
+      inreplace "python/MDSplus/pyproject.toml", "[tool.setuptools.package-data]",
+        "[tool.setuptools.package-data]\n\t'MDSplus.tests'=['devices/*', 'images/*', 'trees/*', 'mdsip.hosts']\n"
       args = args + %W[
         -DBUILD_TESTING=ON
-      ] 
+      ]
+    else
+      inreplace "python/MDSplus/pyproject.toml", "'MDSplus.tests'", "# 'MDSplus.tests'"
     end
 
     inreplace [
@@ -91,10 +99,10 @@ class Mdsplus < Formula
       ], '/usr/local/mdsplus', opt_prefix
 
 
-
     system "cmake", *args
     system "cmake", "--build", "workspace/build", "--", "-j#{ENV.make_jobs}"
     system "cmake", "--install", "workspace/build", "--prefix", "#{prefix}"
+
 
     # prepend #!/bin/sh to these files so the get the right permissions
     # after homebrew "cleans up"
@@ -105,14 +113,18 @@ class Mdsplus < Formula
                 s.sub!(/\A(?!#!)/, "#!/bin/sh\n")
               end
 
-    # install didn't install python tests.. so comment them out
-    if build.without? "tests" 
-      inreplace prefix/"python/MDSplus/pyproject.toml", "'MDSplus.tests'", "# 'MDSplus.tests'"
+    # add python tests in final install (though cmake would not include them)
+    if build.with? "tests"
+      (prefix/"python/MDSplus").install Dir["python/MDSplus/tests"]
+      rm prefix/"python/MDSplus/tests/Makefile.am"
+      rm prefix/"python/MDSplus/tests/CMakeLists.txt"
     end
+    
+    # build python wheel 
     build_venv = virtualenv_create(buildpath/"venv", "python3.13")
     build_venv.pip_install "wheel"
     ENV.prepend_path "PATH", buildpath/"venv/bin"
-    system "python3", "-m", "pip", "wheel", #"--no-deps", 
+    system "python3", "-m", "pip", "wheel", "--no-deps", 
       "-w", prefix/"python", prefix/"python/MDSplus"
   end
 
@@ -131,13 +143,54 @@ class Mdsplus < Formula
 
   test do
     # `test do` will create, run in and delete a temporary directory.
-    ENV['MDSPLUS_DIR']=opt_prefix
+
+    # setup python
     test_venv = virtualenv_create(testpath/"venv", "python3.13")
     ENV.prepend_path "PATH", testpath/"venv/bin"
+    # image library
+    test_venv.pip_install "pillow" 
     wheel = Dir[opt_prefix/"python/*.whl"].first
     system "python3", "-m", "pip", "install", wheel
-    system "python3", "-c", "from unittest import TextTestRunner;" +
-          "from MDSplus.tests import data_case as test_case;" +
-          "TextTestRunner(verbosity=2).run(test_case.Tests.getTestSuite())"
+
+    # setup MDSplus environment
+    ENV['MDSPLUS_DIR']=opt_prefix
+    # do normal mdsplus/setup.sh stuff
+    shell_output("source #{opt_prefix}/setup.sh && printenv").each_line do |line|
+      key, value = line.chomp.split('=', 2)
+      ENV[key] = value if key && !key.empty?
+    end
+
+    # get python tests resource directory
+    test_dir = shell_output('python -c "from importlib.resources import files; print(files(\"MDSplus.tests\"))"').chomp
+    #puts test_dir
+
+    # add test devices and trees
+    ENV.prepend_path "MDS_PYDEVICE_PATH", test_dir+"/devices"
+    # can't use ENV prepend_*path because MDSplus uses semicolons instead of colons.. sigh
+    ENV['default_tree_path']=(testpath/"trees").to_s+";"+test_dir+"/trees;"+(opt_prefix/"trees").to_s
+    ENV['main_path']=opt_prefix/"trees"
+    ENV['subtree_path']=opt_prefix/"trees/subtree"
+
+    #output = shell_output("printenv")
+    #puts output
+
+    output = shell_output('python3 -c "from MDSplus.tests import exception_case as t; t.Tests.runTests()" 2>&1')
+    assert_match "OK (skipped=1)", output.lines.last.chomp
+    output = shell_output('python3 -c "from MDSplus.tests import tree_case as t; t.Tests.runTests()" 2>&1')
+    assert_match "OK", output.lines.last.chomp
+    output = shell_output('python3 -c "from MDSplus.tests import data_case as t; t.Tests.runTests()" 2>&1')
+    assert_equal "OK", output.lines.last.chomp
+    output = shell_output('python3 -c "from MDSplus.tests import devices_case as t; t.Tests.runTests()" 2>&1')
+    puts output
+    #assert_equal "OK", output.lines.last.chomp
+    assert_match "OK", output.lines[-2].chomp
+    output = shell_output('python3 -c "from MDSplus.tests import segment_case as t; t.Tests.runTests()" 2>&1')
+    puts output
+    #assert_equal "OK", output.lines.last.chomp
+
+    output = shell_output('python3 -c "from MDSplus.tests import dcl_case as t; t.Tests.runTests()" 2>&1')
+    puts output
+    assert_equal "OK", output.lines.last.chomp
+    
   end
 end
