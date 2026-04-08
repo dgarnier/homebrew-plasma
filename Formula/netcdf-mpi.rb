a4c2f20c50b3dd54f6a5a0208b3452dea8430a16
Darren Garnier <garnier@mit.edu>
2026-04-08 19:00:15 -0400

    add conflict and style

---
+++ Formula/netcdf-mpi.rb
@@ -0,0 +1,66 @@
+class NetcdfMpi < Formula
+  desc "Libraries and data formats for array-oriented scientific data"
+  homepage "https://www.unidata.ucar.edu/software/netcdf/"
+  url "https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.10.0.tar.gz"
+  sha256 "ce160f9c1483b32d1ba8b7633d7984510259e4e439c48a218b95a023dc02fd4c"
+  license "BSD-3-Clause"
+  compatibility_version 1
+  head "https://github.com/Unidata/netcdf-c.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^(?:netcdf[._-])?v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "cmake" => :build
+  depends_on "hdf5-mpi"
+  depends_on "open-mpi"
+
+  uses_from_macos "m4" => :build
+  uses_from_macos "bzip2"
+  uses_from_macos "curl"
+  uses_from_macos "libxml2"
+
+  on_macos do
+    depends_on "libaec"
+    depends_on "zstd"
+  end
+
+  conflicts_with "netcdf", because: "both install nc-config and libraries"
+
+  def install
+    args = %w[-DNETCDF_ENABLE_TESTS=OFF -DNETCDF_ENABLE_HDF5=ON -DNETCDF_ENABLE_DOXYGEN=OFF]
+    # Fixes "relocation R_X86_64_PC32 against symbol `stderr@@GLIBC_2.2.5' can not be used" on Linux
+    args << "-DCMAKE_POSITION_INDEPENDENT_CODE=ON" if OS.linux?
+    args << "-DNETCDF_ENABLE_PARALLEL_TESTS=ON"
+    args << "-DNETCDF_MPIEXEC='mpirun'"
+
+    ENV["CC"] = "mpicc"
+    system "cmake", "-S", ".", "-B", "build_shared", *args, "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    system "cmake", "--build", "build_shared"
+    system "cmake", "--install", "build_shared"
+    system "cmake", "-S", ".", "-B", "build_static", *args, "-DBUILD_SHARED_LIBS=OFF", *std_cmake_args
+    system "cmake", "--build", "build_static"
+    lib.install "build_static/libnetcdf.a"
+
+    # Remove shim paths
+    # inreplace [bin/"nc-config", lib/"pkgconfig/netcdf.pc", lib/"cmake/netCDF/netCDFConfig.cmake",
+    #            lib/"libnetcdf.settings"], Superenv.shims_path/ENV.cc, ENV.cc
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include "netcdf_meta.h"
+      int main()
+      {
+        printf(NC_VERSION);
+        return 0;
+      }
+    EOS
+
+    system "mpicc", "test.c", "-L#{lib}", "-I#{include}", "-lnetcdf",
+                   "-o", "test"
+    assert_equal version.to_s, `./test`
+  end
+end
